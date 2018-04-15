6df93171183b21ee077dd99899723d775f168180
Sushant <rahul.kr.sushant@gmail.com>
2018-04-15 06:20:16 +0000

    Safetynet Hax

--- aosp_condor.mk
+++ aosp_condor.mk
@@ -10,3 +10,8 @@ PRODUCT_MODEL := Moto E
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 540
+
+#Device fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys \
+    PRIVATE_BUILD_DESC="condor-user 5.1 LPC23.13-34.8 12 release-keys"
