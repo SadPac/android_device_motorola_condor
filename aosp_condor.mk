a1f9dff3beba99995c4a46b216e29a204f969b3c
razorrahul123 <rahul.kr.sushant@gmail.com>
2018-05-01 19:32:32 +0530

    aoscp bringup

--- aosp_condor.mk
+++ aosp_condor.mk
@@ -1,12 +1,15 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_condor
+PRODUCT_DEVICE := potter
+PRODUCT_NAME := aoscp_condor
 PRODUCT_RELEASE_NAME := MOTO E
 PRODUCT_MODEL := Moto E
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 540
